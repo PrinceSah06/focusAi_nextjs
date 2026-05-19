2ff35aac60dedb71433ffc23005cb2fe1b14b3c3
prince sah <sahp4088@gmial.com>
2026-05-19 11:49:27 +0530

    fix: add prisma config  .evn error

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,11 +3,16 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is missing");
+}
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
+  
   datasource: {
     url: process.env["DATABASE_URL"] ?? "",
   },
